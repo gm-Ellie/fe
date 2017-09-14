6d5732b16e0d565a699521f911f94b20dd54b2d3
gaoman04 <gaoman04@meituan.com>
2017-09-14 13:28:18 +0800

    增加select

---
+++ src/views/Home.vue
@@ -0,0 +1,98 @@
+
+<template>
+  <div>
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="form-vertical">
+          <div class="row">
+            <div class="col-md-2 form-group">
+              <label>出发地</label>
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                  v-for="item in startOpt"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </div>
+            <div class="col-md-2 form-group">
+              <label>目的地</label>
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in startOpt"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option> 
+              </el-select>
+            </div>
+            <div class="col-md-2 form-group">
+              <label>途径地点</label>
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in startOpt"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                </el-option>
+              </el-select>
+              
+            </div>
+            <div class="col-md-2 form-group">
+              <label>乘车日期</label>
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in startOpt"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                </el-option>
+              </el-select>  
+            </div>
+            <div class="col-md-2 form-group">
+              <label>目的地</label>
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                    v-for="item in startOpt"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                </el-option>
+              </el-select>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      startOpt: [
+        {
+          value: 1,
+          label: '北京'
+        }, {
+          value: 2,
+          label: '保定'
+        }
+      ],
+      value: ''
+    }
+  },
+  components: {
+  }
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
